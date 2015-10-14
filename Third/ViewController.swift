8a30b68dc676fa8c5e65a63a15b59d575dd1af35
Dmitry Roytman <d@geans.ru>
2015-10-14 18:51:34 +0500

    Initial Commit

---
+++ Third/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Third
+//
+//  Created by Dmitriy Roytman on 14.10.15.
+//  Copyright © 2015 Dmitriy Roytman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
